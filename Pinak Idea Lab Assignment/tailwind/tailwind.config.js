f3dc9054d09af010ae77f64c04d0ac0b4d9c6e88
Srijagatheeswaran.R <102983197+srijagatheeswaran@users.noreply.github.com>
2024-10-17 00:32:27 +0530

    project complete

---
+++ Pinak Idea Lab Assignment/tailwind/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-blue': '#2A1768', 
+      },
+    },
+  },
+  plugins: [],
+}
+
